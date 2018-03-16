762c9ce7bac926d0a07fc37e8f54368f7006c9b9
Vuyisile Ndlovu <terrameijar@gmail.com>
2018-03-16 18:58:44 +0200

    Script can now be imported or run interactively

--- capitals.py
+++ capitals.py
@@ -3,6 +3,7 @@
 from sys import argv
 import argparse
 
+TTY = False  # Flag will be set to true when running script interactively
 
 def parse_arguments(arg):
     # Handles Commandline input
@@ -42,17 +43,26 @@ def get_list_of_countries():
 def capital(country):
     countries = get_list_of_countries()
     usr_country = country
-
-    try:
-        if usr_country.istitle():
-            print("The Capital city of %s is %s." %
-                  (usr_country, countries[usr_country]))
-        else:
-            usr_country = usr_country.title()
-            print("The Capital City of %s is %s." %
-                  (usr_country, countries[usr_country]))
-    except:
-        print("Please enter a valid country name.")
+    if TTY:
+        try:
+            if usr_country.istitle():
+                print("The Capital city of %s is %s." %
+                      (usr_country, countries[usr_country]))
+            else:
+                usr_country = usr_country.title()
+                print("The Capital City of %s is %s." %
+                      (usr_country, countries[usr_country]))
+        except:
+            print("Please enter a valid country name.")
+    else:
+        try:
+            if usr_country.istitle():
+                return countries[usr_country]
+            else:
+                usr_country = usr_country.title()
+                return countries[usr_country]
+        except:
+            return 0
 
 
 def main(country_arg):
@@ -60,5 +70,6 @@ def main(country_arg):
 
 
 if __name__ == "__main__":
-    # Everything except the script name is passed to get_capital
+    # Everything except the script name is passed to main
+    TTY = True
     main(argv[1:])
